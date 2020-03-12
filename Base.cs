80b8bb12ba5fbff62339f7d1cad8025d92a5425e
MohammedNasir2401 <61683029+MohammedNasir2401@users.noreply.github.com>
2020-03-12 19:37:10 +0400

    Add files via upload

---
+++ Base.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project
+{
+    abstract class Base
+    {
+        protected Texture2D image;
+        protected Color color = Color.White;  //Set the image color white to make it transparent
+        public Vector2 Position,Velocity;
+        public float Orientation;
+        public float Radius = 20;  //Radius is used to detect collisions around
+        public bool IsDead;  // Checks whether the player is dead or alive
+
+
+        public Vector2 Size
+        {
+            get
+            {
+        return image == null ? Vector2.Zero : new Vector2(image.Width, image.Height);
+            }
+        }
+
+        public abstract void Update();
+
+        public virtual void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(image, Position, null, color, Orientation, Size / 2f, 1f, 0, 0);
+
+        }
+    }
+}
---
+++ Second.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project
+{
+   static class Second
+    {
+        static List<Base> items = new List<Base>();   //  List to store all of the items used in the game
+        static bool update;
+        static List<Base> addeditems = new List<Base>();  // Second list to store updated items
+        public static int Count { get { return items.Count; } }  //Count the number of properties
+        public static void Add(Base item)
+        { if (!update)               //Checks whether the list has been updated
+                items.Add(item);  //Adds the items to the list
+            else
+                addeditems.Add(item); //If updated, it adds to the "Updated List"
+
+        }
+        public static void Update()
+        {
+            update = true;
+            foreach (var item in items)    // Checks for the items in the first list
+                item.Update();
+            update = false;
+            foreach (var item in addeditems)  //Checks whether the items have been added to the second list
+               items.Add(item);
+
+            addeditems.Clear();
+            items = items.Where(x => !x.IsDead).ToList();  // Removes any dead entities
+        }
+
+        public static void Draw(SpriteBatch spriteBatch)
+        {
+            foreach (var item in items)
+                item.Draw(spriteBatch);         //Adds the items on the game
+        }
+
+    }
+}
